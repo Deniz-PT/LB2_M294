17201b1ea5244a6703fdbc00ec50430043e2c023
Deniz Ugur <deniz.ugur2006@hotmail.com>
2023-04-11 16:42:45 +0200

    requests done

--- test/script.js
+++ test/script.js
@@ -4,62 +4,91 @@ const newTaskInput = document.getElementById("new-task-input");
 
 let tasks = [];
 
-function renderTaskList() {
-  taskList.innerHTML = "";
-  tasks.forEach((task, index) => {
-    const li = document.createElement("li");
-    li.innerHTML = `
-      <span>${task}</span>
-      <div>
-        <button class="edit-task-btn" data-task-index="${index}">Bearbeiten</button>
-        <button class="delete-task-btn" data-task-index="${index}">Löschen</button>
-      </div>
-    `;
-    taskList.appendChild(li);
-  });
-
-  const editTaskBtns = document.querySelectorAll(".edit-task-btn");
-  editTaskBtns.forEach((btn) => {
-    btn.addEventListener("click", handleEditTaskClick);
-  });
-
-  const deleteTaskBtns = document.querySelectorAll(".delete-task-btn");
-  deleteTaskBtns.forEach((btn) => {
-    btn.addEventListener("click", handleDeleteTaskClick);
-  });
-}
-
 function handleAddTaskSubmit(event) {
   event.preventDefault();
 
   const newTask = newTaskInput.value.trim();
-  if (newTask === "") {
-    alert("Bitte geben Sie eine Aufgabe ein.");
-    return;
-  }
 
   tasks.push(newTask);
   newTaskInput.value = "";
-  renderTaskList();
-}
-
-function handleEditTaskClick(event) {
-  const taskIndex = event.target.dataset.taskIndex;
-  const taskText = tasks[taskIndex];
-  const newTaskText = prompt("Bearbeiten Sie die Aufgabe:", taskText);
-  if (newTaskText === null) {
-    return;
-  }
-  tasks[taskIndex] = newTaskText.trim() === "" ? taskText : newTaskText.trim();
-  renderTaskList();
-}
-
-function handleDeleteTaskClick(event) {
-  const taskIndex = event.target.dataset.taskIndex;
-  tasks.splice(taskIndex, 1);
-  renderTaskList();
 }
 
 addTaskForm.addEventListener("submit", handleAddTaskSubmit);
 
-renderTaskList();
+async function getTasks() {
+  const response = await fetch("http://localhost:3011/tasks", {
+    method: "GET",
+  });
+  const json = await response.json();
+  renderTasks(json);
+}
+
+async function deleteTask(id) {
+  const response = await fetch(`http://localhost:3011/task/${id}`, {
+    method: "DELETE",
+  });
+  const json = await response.json();
+
+  console.log(json);
+  getTasks();
+}
+
+async function addTask() {
+  const title = newTaskInput.value;
+  const response = await fetch("http://localhost:3011/tasks", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      title: title,
+    }),
+  });
+
+  getTasks();
+}
+
+async function editTask(id) {
+  const eingabe = prompt("geben sie ihren neue task ein");
+  const response = await fetch(`http://localhost:3011/tasks`, {
+    method: "PUT",
+    headers: {
+      "content-type": "application/json",
+    },
+
+    body: JSON.stringify({
+      id: id,
+      title: eingabe,
+    }),
+  });
+  const json = await response.json();
+
+  console.log(json);
+  getTasks();
+}
+
+function renderTasks(tasks) {
+  taskList.replaceChildren();
+
+  Array.from(tasks).forEach(function (item) {
+    const liElement = document.createElement("li");
+    liElement.innerText = item.title;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = '<img  class="img"src="images/trash.png" alt="" />';
+    deleteBtn.addEventListener("click", function () {
+      deleteTask(item.id);
+    });
+
+    const putBtn = document.createElement("button");
+    putBtn.innerHTML = '<img  class="img"src="images/edit.png" alt="" />';
+    putBtn.addEventListener("click", function () {
+      editTask(item.id);
+    });
+
+    liElement.appendChild(putBtn);
+    liElement.appendChild(deleteBtn);
+
+    taskList.append(liElement);
+  });
+}
